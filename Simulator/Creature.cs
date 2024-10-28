ce60dc36d2582533abdb8bcb7b9eb35e49d31837
fusik <jakubfus.priv@gmail.com>
2024-10-28 23:26:37 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,26 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Simulator;
 
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string name = "Unknown";
+    public string Name
+    {
+        get => name;
+        init
+        {
+            name = value.Trim();
+            if (name.Length > 25)
+            {
+                name = name[..25].Trim();
+            }
+            if (name.Length < 3)
+            {
+                name = name.PadRight(3, '#');
+            }
+            if (char.IsLower(name[0]))
+            {
+                name = char.ToUpper(name[0]) + name[1..];
+            }
+        }
+    }
 
-    public Creature(string name, int level = 1)
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        set
+        {
+            if (level == 1)
+            {
+                level = value < 1 ? 1 : value > 10 ? 10 : value;
+            }
+        }
+    }
+
+    public Creature(string name = "Unknown", int level = 1)
     {
         Name = name;
         Level = level;
     }
-    public string Info => $"{Name} <{Level}>";
+
+    public Creature() { }
 
     public void SayHi()
     {
-        Console.WriteLine($"Hi, I am {Name}!");
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
     }
-}
 
+    public void Upgrade()
+    {
+        if (Level < 10)
+        {
+            level++;
+        }
+    }
+
+    public string Info => $"{Name} [{Level}]";
+}
