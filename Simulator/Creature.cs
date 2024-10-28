24b2353c061b55a262b4d63ae0357bbf760ba9a1
fusik <jakubfus.priv@gmail.com>
2024-10-28 09:05:22 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+    public string Info => $"{Name} <{Level}>";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I am {Name}!");
+    }
+}
+
