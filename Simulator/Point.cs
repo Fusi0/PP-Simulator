e014645610c95dde56edb41b446a3ddb8b9686ec
fusik <jakubfus.priv@gmail.com>
2024-11-13 00:51:15 +0100

    Prostokąty

---
+++ Simulator/Point.cs
@@ -0,0 +1,34 @@
+﻿using Simulator;
+using static System.Collections.Specialized.BitVector32;
+
+public readonly struct Point
+{
+    public readonly int X, Y;
+    public Point(int x, int y) => (X, Y) = (x, y);
+    public override string ToString() => $"({X}, {Y})";
+
+    public Point Next(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => new Point(X, Y + 1),
+            Direction.Right => new Point(X + 1, Y),
+            Direction.Down => new Point(X, Y - 1),
+            Direction.Left => new Point(X - 1, Y),
+            _ => default
+        };
+    }
+
+    public Point NextDiagonal(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => new Point(X + 1, Y + 1),
+            Direction.Right => new Point(X + 1, Y - 1),
+            Direction.Down => new Point(X - 1, Y - 1),
+            Direction.Left => new Point(X - 1, Y + 1),
+            _ => default
+        };
+    }
+}
+
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -7,11 +7,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Starting Simulator!\n");
-        Lab4a();
-        Creature c = new Elf("Elandor", 5, 3);
-        Console.WriteLine(c);
-        Lab4b();
+
+        //Console.WriteLine("Starting Simulator!\n");
+        //Lab4a();
+        //Creature c = new Elf("Elandor", 5, 3);
+        //Console.WriteLine(c);
+        //Lab4b();
+
+        Lab5a();
     }
     static void Lab4a()
     {
@@ -62,5 +65,43 @@ internal class Program
             ELF: E## [10][0]
             ORC: Morgash [6][4]
         */
+    }
+
+    static void Lab5a()
+    {
+        Point p = new(10, 25);
+        Rectangle r1 = new Rectangle(2, 2, 6, 7);
+        Console.WriteLine($"Rectangle: {r1}");
+
+        Rectangle r2 = new(4, 5, 2, 3);
+        Console.WriteLine($"Rectangle: {r2}");
+
+        try
+        {
+            Rectangle r3 = new Rectangle(0, 0, 2, 6);
+            Console.WriteLine($"Wrong rectangle: {r3}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Point point1 = new Point(4, 4);
+        Point point2 = new Point(7, 8);
+        Point point3 = new Point(-2, -3);
+        Point point4 = new Point(-10, -15);
+
+        Rectangle r4 = new Rectangle(point1, point2);
+        Console.WriteLine($"Rectangle: {r4}");
+
+        Console.WriteLine("-----");
+
+        Rectangle r5 = new Rectangle(-4, -6, 6, 8);
+        Console.WriteLine($"Rectangle: {r5}");
+
+        Console.WriteLine($"Contains (4,4) - {r5.Contains(point1)}");
+        Console.WriteLine($"Contains (7,8) - {r5.Contains(point2)}");
+        Console.WriteLine($"Contains (-2,-3) - {r5.Contains(point3)}");
+        Console.WriteLine($"Contains (-10,-15) - {r5.Contains(point4)}");
     }
 }
