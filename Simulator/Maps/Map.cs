cf06e48b2f25798d5c6f05b40070273976bdaf1b
fusik <jakubfus.priv@gmail.com>
2024-11-26 16:56:55 +0100

    Let's go

--- Simulator/Maps/Map.cs
+++ Simulator/Maps/Map.cs
@@ -1,10 +1,13 @@
-﻿using System.Drawing;
+﻿
+
+using System.Drawing;
 
 namespace Simulator.Maps
 {
     /// <summary>
     /// Map of points.
     /// </summary>
+
     public abstract class Map
     {
         /// <summary>
@@ -15,16 +18,22 @@ namespace Simulator.Maps
         public int SizeX { get; }
         public int SizeY { get; }
         private Rectangle bounds;
+        protected abstract List<Creature>?[,] Fields { get; }
+
         protected Map(int sizeX, int sizeY)
         {
-            if (this.SizeX < 5 || SizeY < 5)
+
+            if (sizeX < 5 || sizeY < 5)
             {
                 throw new ArgumentOutOfRangeException(nameof(sizeX), "Mapa za mała");
             }
-            this.SizeX = sizeX;
+
+            SizeX = sizeX;
             SizeY = sizeY;
 
             bounds = new Rectangle(0, 0, this.SizeX - 1, SizeY - 1);
+
+
         }
         /// 
         public virtual bool Exist(Point p)
@@ -48,5 +57,66 @@ namespace Simulator.Maps
         /// <param name="d">Direction.</param>
         /// <returns>Next point.</returns>
         public abstract Point NextDiagonal(Point p, Direction d);
+        public void Add(Creature creature, Point position)
+        {
+
+            int x = position.X;
+            int y = position.Y;
+
+            if (Fields[x, y] == null)
+            {
+                Fields[x, y] = new List<Creature>();
+            }
+
+            Fields[x, y]!.Add(creature);
+        }
+
+        public void Remove(Creature creature, Point position)
+        {
+            int x = position.X;
+            int y = position.Y;
+
+            var creatures = Fields[x, y];
+            if (creatures != null)
+            {
+                creatures.RemoveAll(c => c == creature);
+
+                if (creatures.Count == 0)
+                {
+                    Fields[x, y] = null;
+                }
+            }
+        }
+
+        public void Move(Creature creature, Point position, Direction direction)
+        {
+            int x = position.X;
+            int y = position.Y;
+
+            Remove(creature, position);
+
+
+            var newPosition = Next(position, direction);
+
+
+            Add(creature, newPosition);
+        }
+
+        public List<Creature> At(Point position)
+        {
+            int x = position.X;
+            int y = position.Y;
+
+            var creaturesAtPoint = new List<Creature>();
+
+            var creatures = Fields[x, y];
+            if (creatures != null)
+            {
+                creaturesAtPoint.AddRange(creatures);
+            }
+
+            return creaturesAtPoint;
+        }
+
     }
 }
--- Simulator/Maps/SmallMap.cs
+++ Simulator/Maps/SmallMap.cs
@@ -8,16 +8,20 @@ namespace Simulator.Maps
 {
     public abstract class SmallMap : Map
     {
+        private readonly List<Creature>?[,] fields;
         protected SmallMap(int sizeX, int sizeY) : base(sizeX, sizeY)
         {
             if (sizeX > 20)
             {
-                throw new ArgumentOutOfRangeException(nameof(sizeX), "Mapa za wysoka");
+                throw new ArgumentOutOfRangeException(nameof(sizeX), "Mapa za duża");
             }
             if (sizeY > 20)
             {
-                throw new ArgumentOutOfRangeException(nameof(sizeY), "Mapa za szeroka");
+                throw new ArgumentOutOfRangeException(nameof(sizeY), "Mapa za duża");
             }
+            fields = new List<Creature>?[sizeX, sizeY];
         }
+        protected override List<Creature>?[,] Fields => fields;
+
     }
 }
